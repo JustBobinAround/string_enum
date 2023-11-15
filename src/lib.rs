d4719ad00962e0d63a7e87423f393c17f8043267
JustBobinAround <roberthenryjunkins@gmail.com>
2023-11-15 16:48:48 -0500

    added to_str method

--- src/lib.rs
+++ src/lib.rs
@@ -7,14 +7,23 @@ pub fn string_enum(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as ItemEnum);
     let enum_name = &input.ident;
     let variants = input.variants.iter().map(|variant| &variant.ident);
+    let variants_2 = variants.clone();
     let mut formated_variants = String::new();
+    let mut string_cases = String::new();
 
     for variant in variants {
         formated_variants = format!("{}stringify!({})=> Some({}::{}) ,\n", formated_variants, variant, enum_name, variant);
     }
+    for variant in variants_2 {
+        string_cases = format!("{}{}::{} => stringify!({}) ,\n", string_cases, enum_name, variant, variant);
+    }
 
     //well this was weird
     let expanded = format!(r#"
+        trait StringEnum {{
+            pub fn str_match(s: &str) -> Option<Self>;
+            pub fn to_str(&self) -> String;
+        }}
         impl {} {{
             pub fn str_match(s: &str) -> Option<Self> {{
                 match s {{
@@ -22,8 +31,17 @@ pub fn string_enum(input: TokenStream) -> TokenStream {
                     _ => None,
                 }}
             }}
+
+            pub fn to_str(&self) -> String {{
+                match *self {{
+                    {}
+                    _ => None,
+                }}
+            }}
         }}
-        "#, enum_name, formated_variants);
+        "#, enum_name, formated_variants, string_cases);
 
     expanded.parse().unwrap()
 }
+
+
